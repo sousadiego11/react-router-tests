3d09425fc2b0497e05ca4156c6aa840eea1a2bdd
Diego Sousa <65742620+sousadiego11@users.noreply.github.com>
2021-02-06 23:02:05 -0300

    up

---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = (props)=> {
+  console.log(props)
+  
+  return (
+    <div>
+      Home component
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/posts.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Posts = (props)=> {
+  console.log(props)
+  
+  return (
+    <div>
+      Posts component
+    </div>
+  );
+}
+
+export default Posts;
